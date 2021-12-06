9af7c0b6e2333e6c886d82bb222cba2bf3704b0f
Kenny.ch <meier_david_91@hotmail.com>
2021-12-06 07:58:44 +0100

    Update createSSL.sh

--- scripts/helpers/createSSL.sh
+++ scripts/helpers/createSSL.sh
@@ -8,4 +8,4 @@ mkdir -p ~/.ssh/
 openssl req -new -x509 -newkey rsa:4096 -keyout ~/.ssh/module-private.key -outform DER -out ~/.ssh/module-public.key -nodes -days 3650 -subj "/CN=kernel-module"
 echo "The following password will need to be used in enroll MOK on your next startup."
 sudo mokutil --import ~/.ssh/module-public.key
-sudo ./*$1.run --module-signing-secret-key=$HOME/.ssh/module-private.key --module-signing-public-key=$HOME/.ssh/module-public.key -q
+sudo ./*$0.run --module-signing-secret-key=$HOME/.ssh/module-private.key --module-signing-public-key=$HOME/.ssh/module-public.key -q
