d461ed36acd08a067081873a2a5c4ef05bed4b74
Arthur Rasmusson <arthur@arccompute.com>
2022-01-04 15:23:51 -0500

    Added further improvements to comments.

--- src/libvfio/types/hardware.nim
+++ src/libvfio/types/hardware.nim
@@ -8,43 +8,43 @@ import strutils
 import os
 
 type
-  VfioTypes* = enum          ## Enumeration to determine the types of VFIOs.
-    vtUNA = (0x00, "UNA"),   ## 0x00 is the unassigned class.
-    vtNET = (0x02, "NET"),   ## 0x02 is the class for networking devices.
-    vtGPU = (0x03, "GPU")    ## 0x03 is the class for display devices.
+  VfioTypes* = enum                           ## Enumeration to determine the types of VFIOs.
+    vtUNA = (0x00, "UNA"),                    ## 0x00 is the unassigned class.
+    vtNET = (0x02, "NET"),                    ## 0x02 is the class for networking devices.
+    vtGPU = (0x03, "GPU")                     ## 0x03 is the class for display devices.
 
-  Cpu* = object              ## CPU configuration object.
-    cores*: int              ## How many cores each virtual CPU has.
-    sockets*: int            ## How many virtual CPUs we put in the VM.
-    threads*: int            ## How many threads each core has.
-    ramAlloc*: int           ## Megabytes of RAM required for the VM.
+  Cpu* = object                               ## CPU configuration object.
+    cores*: int                               ## How many cores each virtual CPU has.
+    sockets*: int                             ## How many virtual CPUs we put in the VM.
+    threads*: int                             ## How many threads each core has.
+    ramAlloc*: int                            ## Megabytes of RAM required for the VM.
 
-  Vfio* = object             ## VFIO structure for handling different VFIOs.
-    deviceName*: string      ## PCI device address
-    base*: string            ## Driver override address
-    case kind: VfioTypes     ## Private enumeration value stating which
-                             ##  version to use.
+  Vfio* = object                              ## VFIO structure for handling different VFIOs.
+    deviceName*: string                       ## PCI device address
+    base*: string                             ## Driver override address
+    case kind: VfioTypes                      ## Private enumeration value stating which
+                                              ##  version to use.
     of vtNET:
-      mac*: string           ## MAC Address to pass into VM.
+      mac*: string                            ## MAC Address to pass into VM.
     of vtGPU:
-      virtNum*: int          ## What virtual function is this mapped to.
-      gpuType*: string       ## Type of the GPU.
-      vRam*: int             ## vRAM available to the GPU.
+      virtNum*: int                           ## What virtual function is this mapped to.
+      gpuType*: string                        ## Type of the GPU.
+      vRam*: int                              ## vRAM available to the GPU.
     else: nil
 
   Mdev* = object
-    uuid*: string            ## UUID to access the mdev device
-    devId*: string           ## Device name
-    ram*: int                ## RAM for the MDev device
-    stop*: string            ## Path for removal.
+    uuid*: string                             ## UUID to access the mdev device
+    devId*: string                            ## Device name
+    ram*: int                                 ## RAM for the MDev device
+    stop*: string                             ## Path for removal.
 
 const
-  PassableVfios* = @[        ## List of PCI classes we can pass into the Arc
-                             ##  containers.
+  PassableVfios* = @[                         ## List of PCI classes we can pass into the Arc
+                                              ##  containers.
     ord(vtNET), ord(vtGPU)
   ]
 
-func `$`*(x: Cpu): string =  ## Overload for printing out a CPU
+func `$`*(x: Cpu): string =                   ## Overload for printing out a CPU
   &"cores={x.cores},threads={x.threads},sockets={x.sockets}"
 
 func `$`*(x: Vfio): string = ## Overload for printing out a VFIO.
@@ -83,10 +83,10 @@ func getVfio*(k: int, l: seq[string], dir: string): Option[Vfio] =
     # NOTE: 7 is a magic number due to how GIM works.
     if len(l) == 7 and startsWith(l[0], "VF"): # We only get the top level GPU
       let
-        gpuType = split(l[1], ":")[1] # Gets the type of the GPU.
+        gpuType = split(l[1], ":")[1]         ## Gets the type of the GPU.
         size = parseInt(split(split(l[5], ":")[1], " ")[0]) # Gets size of
                                                             #  GPU.
-        nameList = split(l[2], ":") # Gets the Device Id.
+        nameList = split(l[2], ":")           ## Gets the Device Id.
         name = join(nameList[1 .. 3], ":")
 
       vfio = Vfio(kind: vtGPU)
@@ -100,8 +100,8 @@ func getVfio*(k: int, l: seq[string], dir: string): Option[Vfio] =
     else: result = none(Vfio)
   else: result = none(Vfio)
 
-func isNet*(x: Vfio): bool = x.kind == vtNET ## Helper function to figure out
-                                             ##  if the VFIO is a network
-                                             ##  device.
-func isGpu*(x: Vfio): bool = x.kind == vtGPU ## Helper function to figure out
-                                             ##  if the VFIO is a GPU device.
+func isNet*(x: Vfio): bool = x.kind == vtNET  ## Helper function to figure out
+                                              ##  if the VFIO is a network
+                                              ##  device.
+func isGpu*(x: Vfio): bool = x.kind == vtGPU  ## Helper function to figure out
+                                              ##  if the VFIO is a GPU device.
