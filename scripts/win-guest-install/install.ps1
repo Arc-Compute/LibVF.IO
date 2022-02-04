f4699e47712fe4704608fbf39155dc6b3cf5ac1b
arthurrasmusson <arthur@arccompute.io>
2022-02-04 17:37:36 -0500

    Added sshd_config and ssh file permission fixes.

--- scripts/win-guest-install/install.ps1
+++ scripts/win-guest-install/install.ps1
@@ -6,6 +6,12 @@ cd $env:USERPROFILE\temp-install\
 # Extract guestutil zip files.
 Get-ChildItem $env:USERPROFILE\temp-install\ -Filter *.zip | Expand-Archive -DestinationPath $env:USERPROFILE\temp-install\ -Force
 
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install sed
+choco install sed -Y
+
 # Install SSH.
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
@@ -15,6 +21,7 @@ Start-Service sshd
 
 # Set sshd service to start automatically on boot.
 Set-Service -Name sshd -StartupType 'Automatic'
+Set-Service -Name ssh-agent -StartupType 'Automatic'
 
 # Allow sshd through Windows firewall on port 22 if it isn't already allowed.
 if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
@@ -30,6 +37,10 @@ cp $env:USERPROFILE\temp-install\authorized_keys $env:USERPROFILE\.ssh\
 
 # Fix SSH authorized_keys permissions.
 icacls.exe $env:USERPROFILE\.ssh\authorized_keys /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+
+# Update C:\ProgramData\ssh\sshd_config (remove Match Group administrators lines)
+sed -i 's/Match Group administrators/#Match Group administrators/g' C:\ProgramData\ssh\sshd_config
+sed -i 's/AuthorizedKeysFile __PROGRAMDATA__/#AuthorizedKeysFile __PROGRAMDATA__/g' C:\ProgramData\ssh\sshd_config
 
 # Restart sshd. 
 Restart-Service sshd
