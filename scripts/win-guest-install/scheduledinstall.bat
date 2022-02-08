ad8f2daaf061bff7a3fae355665c9b4dd255dab3
arthurrasmusson <arthur@arccompute.io>
2022-02-07 20:54:21 -0500

    WIP: Changed method for invoking installation powershell.

--- scripts/win-guest-install/scheduledinstall.bat
+++ scripts/win-guest-install/scheduledinstall.bat
@@ -1,2 +1,1 @@
-cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted"
-SCHTASKS /CREATE /SC ONLOGON /TN "install" /TR "cmd.exe /c powershell -File D:\start-install.bat" /RL HIGHEST /RU ADMINISTRATOR
+SCHTASKS /CREATE /SC ONLOGON /TN "install" /TR "cmd.exe /c powershell -File D:\start-install.bat" /RL HIGHEST
--- scripts/win-guest-install/start-install.bat
+++ scripts/win-guest-install/start-install.bat
@@ -1,1 +1,2 @@
+cmd.exe /c powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted"
 cmd.exe /c powershell -Command "Start-Process powershell -verb RunAs -ArgumentList "D:\install.ps1""
