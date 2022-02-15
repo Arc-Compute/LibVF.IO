67bd89f28e7b15288ec59e77bbaceeaeba38860a
arthurrasmusson <arthur@arccompute.io>
2022-02-15 15:24:00 -0500

    WIP: Added SMB registry tweak.

--- scripts/win-guest-install/install.ps1
+++ scripts/win-guest-install/install.ps1
@@ -78,6 +78,7 @@ powercfg /x -standby-timeout-dc 0
 # Verify guest GPU driver (force install if vendorid == 10de && no driver).
 
 # Mount Samba permanently at Z:\.
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters\" -Name "AllowInsecureGuestAuth" -Value 1
 New-PSDrive -Name 'Z' -Root '\\10.0.2.2\Public Files' -Persist -PSProvider Filesystem
 
 # Prompt user for disabling explorer.exe (Y/n)?
